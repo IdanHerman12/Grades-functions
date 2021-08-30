a8cc7d43bd0963c39f496abb64c25a81ae7f966f
IdanHerman12 <idanh156@gmail.com>
2021-08-30 15:45:12 +0300

    function fine the meidan grade

--- excerice.js
+++ excerice.js
@@ -1,15 +1,17 @@
 'use strict';
-const arr = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
-console.log(arr.sort());
+const arr = [73, 91, 88, 56, 62, 84, 95, 96, 88,92];
+console.log(arr.sort((a,b)=> b-a));
 //he range of the grades (difference between highest and lowest)
-function Range(grades){
-  let localArr= grades.sort();
-  let max=localArr[localArr.length-1];
-  let min=localArr[0];
+function RangeGrades(grades){
+  let localArr= grades.sort((a,b)=> b-a);
+  let max=localArr[0];
+  let min=localArr[localArr.length-1];
   console.log("range="+(max-min));
 }
-Range(arr);
-
+RangeGrades(arr);
+//finding the the median grade (the grade in the middle)
 function medianGrade(grades){
-    
+    let localArr= grades.sort((a,b)=> b-a);
+    let mid=(grades.length-1)/2;
+    console.log("middle grade="+localArr[Math.floor(mid)]);
 }
