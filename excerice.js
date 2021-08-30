35677cb58eff3963a54e4e5027cfd61984d537ca
IdanHerman12 <idanh156@gmail.com>
2021-08-30 14:59:22 +0300

    one function  of Range coplete

--- excerice.js
+++ excerice.js
@@ -1,1 +1,15 @@
+'use strict';
+const arr = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
+console.log(arr.sort());
+//he range of the grades (difference between highest and lowest)
+function Range(grades){
+  let localArr= grades.sort();
+  let max=localArr[localArr.length-1];
+  let min=localArr[0];
+  console.log("range="+(max-min));
+}
+Range(arr);
 
+function medianGrade(grades){
+    
+}
